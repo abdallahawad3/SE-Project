039384069b57e3ce63607ca5fdc8411b32b8542a
Omar Abdelaty <omarzayed489@gmail.com>
2024-05-13 00:07:21 +0300

    displayallUserPosts

--- assets/script/ProfileScript.js
+++ assets/script/ProfileScript.js
@@ -59,3 +59,38 @@ async function allUserPosts() {
     displayAllUserPosts(data.data);
   }
 }
+
+function displayAllUserPosts(object) {
+    let temp = ``;
+    object.forEach(element => {
+      temp = `
+      <div class="card container-fluid col-lg-8 container-md mx-auto shadow-lg my-4">
+        <div class="card-header d-flex align-items-center justify-content-between">
+          <div>
+            <img src="${typeof element.author.profile_image == "string" ? element.author.profile_image : 'https://picsum.photos/200/300'}" style="width: 50px; border-radius: 50%; padding: 2px; border: 2px solid gainsboro;" alt="User image">
+            <span class="fw-bold">${typeof element.author.username === "string" ? element.author.username : "username"}</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <img class="w-100" src="${typeof element.image == "string" ? element.image : 'https://picsum.photos/800/600'}" alt="main image">
+          </div>
+          <div class="card-info border-bottom">
+            <h6 class="text-black-50 mt-1">${element.created_at}</h6>
+            <h5>${typeof element.title === "string" ? element.title : "title"}</h5>
+            <p>${typeof element.body === "string" ? element.body : "body"}</p>
+          </div>
+          <div class="card-tags mt-2 d-flex align-items-center gap-3">
+            <div class="comments d-flex align-items-center gap-1">
+              <i class="bi bi-pen"></i>
+              <p class="mb-0">
+                <span>(${element.comments_count}) </span>Comments
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      `
+      userPostsContainer.innerHTML += temp;
+    });
+  }
